91a34f8c8f3192d7b2de6c8cb6080ca01a4ebab3
muke101 <muke@protonmail.com>
2025-02-13 22:30:59 +0000

    fixed trace gen scripts

--- utils/convert_parquet.py
+++ utils/convert_parquet.py
@@ -1,8 +1,16 @@
 import polars as pd
 import csv
 import sys
+import re
 
-csv_inp = csv.DictReader(sys.stdin)
+def filter_csv():
+    pattern = re.compile(r'^\w+,\w+,\w+,\w+$')
+    for line in sys.stdin:
+        line = line.strip()
+        if pattern.fullmatch(line):
+            yield line
+
+csv_inp = csv.DictReader(filter_csv())
 df = pd.DataFrame(list(csv_inp))
 df = df.with_columns([pd.col(col).cast(pd.Int64, strict=False) for col in df.columns])
 df.write_parquet(sys.argv[1])
--- utils/spec_trace_automation.py
+++ utils/spec_trace_automation.py
@@ -11,7 +11,7 @@ base_dir = os.getcwd() # = /mnt/data/checkpoints-expanded/benchmark
 base_run = False
 spec_path = "/work/muke/spec2017/"
 expanded_spec_path = "/work/muke/spec2017-expanded/"
-gem5 = "/work/muke/Branch-Correlations/gem5/"
+gem5 = "/work/muke/Branch-Correlations/trace-gem5/"
 results_dir = "/mnt/data/results/branch-project/traces/"
 label_file_dir = "/work/muke/Branch-Correlations/label_files/"
 workloads = "/work/muke/alberta-workloads/"
@@ -21,7 +21,7 @@ procs = []
 
 def get_bench_flags(run_name):
     #test
-    if run_name.isdigit():
+    if run_name.isdigit() and len(run_name) == 1:
         if benchmark in ["602.gcc_s", "657.xs_s", "648.exchange2_s"]:
             run_dir = expanded_spec_path+"benchspec/CPU/"+benchmark+"/run/modified/run_peak_refspeed_mytest-64.0000/"
         else:
@@ -62,6 +62,7 @@ def get_bench_flags(run_name):
 #iterate over all checkpoint.n dirs
 for out_dir in os.listdir(base_dir):
     run_name = out_dir.split("checkpoints.")[1]
+    print(benchmark, run_name)
     run_dir, command = get_bench_flags(run_name)
     cpt_number = 0
     out_dir = os.path.join(base_dir,out_dir)
@@ -72,17 +73,20 @@ for out_dir in os.listdir(base_dir):
             waited = 0
             finished = False
             cpt_number += 1
-            binary = command.split()[0]
+            binary = "./"+command.split()[0]
             benchmark_name = benchmark.split("_")[0].split(".")[1]
             outdir = results_dir+benchmark_name+"."+run_name+"/raw/"
             if not os.path.exists(outdir): os.makedirs(outdir) #create the parent directories for gem5 stats dir if needed
             outdir += str(cpt_number)+".out"
-            run = gem5+"build/ARM/gem5.fast --outdir="+outdir+" "+gem5+"configs/deprecated/example/se.py --cpu-type=DerivO3CPU --caches --restore-simpoint-checkpoint -r "+str(cpt_number)+" --checkpoint-dir "+out_dir+" --restore-with-cpu=NonCachingSimpleCPU --mem-size=50GB -c "+binary+" --options=\""+' '.join(command.split()[1:])+"\" 2> >(tail -n +7 | python3 /work/muke/Branch-Correlations/utils/convert_parquet.py "+results_dir+benchmark+"."+run_name+".trace)"
+            run = gem5+"build/ARM/gem5.fast --outdir=/dev/null "+gem5+"configs/deprecated/example/se.py --cpu-type=DerivO3CPU --caches --restore-simpoint-checkpoint -r "+str(cpt_number)+" --checkpoint-dir "+out_dir+" --restore-with-cpu=NonCachingSimpleCPU --mem-size=50GB -c "+binary+" --options=\""+' '.join(command.split()[1:])+"\" 2> >(tail -n +7 | python3 /work/muke/Branch-Correlations/utils/convert_parquet.py "+results_dir+benchmark+"."+run_name+".trace)"
             os.chdir(run_dir)
             while psutil.virtual_memory().percent > 60 and psutil.cpu_percent() > 90: time.sleep(60*5)
-            p = Popen(run, shell=True)
+            print(run)
+            print()
+            #p = Popen(run, shell=True)
             os.chdir(base_dir)
-            procs.append(p)
+            #procs.append(p)
+            continue
             while waited < 60*2 and finished == False:
                 time.sleep(10)
                 waited += 10
