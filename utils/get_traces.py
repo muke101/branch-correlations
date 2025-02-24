cc7934860f0424fa5c38a3cde6b4ad19d4a3b04e
muke101 <muke@protonmail.com>
2025-02-24 23:49:55 +0000

    fixed get_traces

--- utils/get_traces.py
+++ utils/get_traces.py
@@ -9,7 +9,7 @@ def get_simpoint_weight(benchmark, workload, checkpoint):
     simpt_file = open(simpoint_dir+benchmark+"."+workload+".simpts", "r")
     simpoints = [(int(s.strip().split()[0]), int(s.strip().split()[1])) for s in simpt_file.readlines()]
     simpoints = sorted(simpoints, key=lambda x: x[0])
-    simpoint_indx = simpoints[checkpoint][1]
+    simpoint_indx = simpoints[int(checkpoint)-1][1]
     weight_file = open(simpoint_dir+benchmark+"."+workload+".weights", "r")
     weights = [w.split()[0] for w in weight_file.readlines()]
     simpt_file.close()
@@ -19,7 +19,6 @@ def get_simpoint_weight(benchmark, workload, checkpoint):
 def get_hdf5_set(benchmark, set_type):
     datasets = []
     test_pattern = r"\d"
-    validate_pattern = r"train\.\d"
 
     for trace in os.listdir(hdf5_dir):
         if not trace.endswith('.hdf5') or not trace.startswith(benchmark): continue
@@ -27,7 +26,9 @@ def get_hdf5_set(benchmark, set_type):
         if set_type == 'test':
             if not re.fullmatch(test_pattern, workload): continue
         elif set_type == 'validate':
-            if not re.fullmatch(validate_pattern, workload): continue
+            if workload == 'train' and trace.split('.')[3].isdigit(): 
+                workload = 'train.'+trace.split('.')[3]
+            else: continue
         elif set_type == 'train':
             if re.search(test_pattern, workload) or re.search(validate_pattern, workload): continue
         else:
@@ -40,7 +41,6 @@ def get_hdf5_set(benchmark, set_type):
 def get_trace_set(benchmark, set_type):
     traces = []
     test_pattern = r"\d"
-    validate_pattern = r"train\.\d"
 
     for trace in os.listdir(trace_dir):
         if not trace.endswith('.trace') or not trace.startswith(benchmark): continue
@@ -48,7 +48,9 @@ def get_trace_set(benchmark, set_type):
         if set_type == 'test':
             if not re.fullmatch(test_pattern, workload): continue
         elif set_type == 'validate':
-            if not re.fullmatch(validate_pattern, workload): continue
+            if workload == 'train' and trace.split('.')[3].isdigit(): 
+                workload = 'train.'+trace.split('.')[3]
+            else: continue
         elif set_type == 'train':
             if re.search(test_pattern, workload) or re.search(validate_pattern, workload): continue
         else:
