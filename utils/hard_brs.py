196aa6e5de97a0cdba09a74c00b499e081c45061
w.yin <w.yin@qmul.ac.uk>
2025-03-10 15:44:27 +0000

    save exceptions too

--- utils/hard_brs.py
+++ utils/hard_brs.py
@@ -5,6 +5,7 @@ cwd = os.getcwd()
 sys.path.insert(0, cwd)
 
 import pickle
+import polars as pl
 from utils.get_traces import benchmarks
 
 hard_branches_dir = "/mnt/data/results/branch-project/h2ps/validate/"
@@ -15,6 +16,7 @@ NUM_BRS_TO_PRINT = 100
 def greedy_select_top_brs(list_inputs, mpki_dicts, sorted_brs, num_brs):
     selected_brs = []
     next_br_idx = [0] * len(list_inputs)
+    exceptions = []
     for i in range(num_brs):
         next_br_total_mpki = [0.0] * len(list_inputs)
         next_br_pc = [0] * len(list_inputs)
@@ -28,11 +30,19 @@ def greedy_select_top_brs(list_inputs, mpki_dicts, sorted_brs, num_brs):
                     total_mpki += mpki_dicts[inppp][br]
                 except KeyError:
                     print("KeyError: [{}] br: {} not in workload: {}".format(i, br, inppp))
+                    exceptions.append({'br': br, 'i': i, 'workload': inppp, 'selected': False})
             next_br_total_mpki[j] = total_mpki
             next_br_pc[j] = br
         max_j = next_br_total_mpki.index(max(next_br_total_mpki))
         selected_brs.append(next_br_pc[max_j])
-    return selected_brs
+
+    ii = 0 
+    for exception in exceptions:
+        if exception['i'] == ii and exception['br'] == selected_brs[ii]:
+            exception['selected'] = True
+            ii += 1
+
+    return selected_brs, exceptions
 
 
 for benchmark in benchmarks:
@@ -41,11 +51,13 @@ for benchmark in benchmarks:
     with open("{}sorted_brs_{}.pkl".format(stats_dir, benchmark), 'rb') as f:
         sorted_brs = pickle.load(f)
     workloads = list(mispred_dicts.keys())
-    brs = greedy_select_top_brs(workloads, mispred_dicts, sorted_brs, NUM_BRS_TO_PRINT)
+    brs, exceptions = greedy_select_top_brs(workloads, mispred_dicts, sorted_brs, NUM_BRS_TO_PRINT)
+    
 
     with open(hard_branches_dir+benchmark, 'w') as f:
         for br in brs:
-            f.write(hex(br)+'\n')
+            f.write(br+'\n')
+
+    pl.DataFrame(exceptions).write_csv(hard_branches_dir+'exceptions/'+benchmark+".csv")
     
     print("finished writing hard branches for {}".format(benchmark))
-
