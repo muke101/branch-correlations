eab468a077784c3485934ad247287e887a21e8bd
muke <muke101@protonmail.com>
2025-03-17 21:41:52 +0000

    added some notes in script for things that are broken

--- utils/get_improved.py
+++ utils/get_improved.py
@@ -21,6 +21,7 @@ stats_dir = "/work/muke/Branch-Correlations/stats/"
 branchnet_results_dir = "/mnt/data/results/branch-project/branchnet-results/main/"
 
 def tage_mispreds(benchmark):
+    #FIXME: msipred_dicts needs to be generated from the test set, probably want to just grab the trace files and iterate here
     with open("{}mispred_dicts_{}.pkl".format(stats_dir, benchmark), 'rb') as f:
         mispred_dicts = pickle.load(f)
     with open("{}sorted_brs_{}.pkl".format(stats_dir, benchmark), 'rb') as f:
@@ -46,7 +47,8 @@ def branchnet_mispreds(benchmark):
             indx = [match.end() for match in re.finditer(re.escape(benchmark), run['simpoint'])][0]
             checkpoint = run['simpoint'][indx:].split('.')[-1]
             workload = run['simpoint'][indx:].split('.')[1]
-            if workload == 'train': workload = 'train.'+run['simpoint'][indx:].split('.')[2]
+            #TODO: do we need to be careful about weighting simpoints across multiple workloads?
+            if len(workload) > 1 and not workload.isdigit(): continue #only select test set
             weight = utils.get_traces.get_simpoint_weight(benchmark, workload, checkpoint)
             correct += float(run['correct']) * weight
             total += float(run['total']) * weight
