6507fdc243ab08543fe71d46131a8ccf86f7609f
muke101 <muke@protonmail.com>
2025-05-09 20:18:16 +0100

    fixed parse branches

--- utils/get_traces.py
+++ utils/get_traces.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import re
 import sys
 from collections import defaultdict
--- utils/parse_branches.py
+++ utils/parse_branches.py
@@ -7,7 +7,7 @@ trace_dir = "/mnt/data/results/branch-project/traces/"
 h2p_dir = "/mnt/data/results/branch-project/h2ps/"
 binary = sys.argv[1]
 r2 = r2pipe.open(binary)
-h2p_file = h2p_dir+binary+"_top100"
+h2p_file = h2p_dir+"641.leela_s_top100"
 
 addrs = [int(i.strip(),16) for i in open(h2p_file).readlines()]
 
@@ -18,25 +18,25 @@ for addr in addrs:
     if inst['type'] != 'cjmp':
         print("Error: H2P type not cjmp. Found: ", inst['type'])
         exit(1)
-    func_addr = inst['fnc_addr']
+    func_addr = inst['fcn_addr']
     for func in r2.cmdj("isj"):
         if func['type'] == "FUNC" and func['vaddr'] == func_addr:
-            print("H2P "+hex(inst['vaddr'])+" is in function "+func['realname'])
+            print("H2P "+hex(addr)+" is in function "+func['realname'])
 
 qsorts = [i for i in r2.cmdj("isj") if i['type'] == "FUNC" and "qsort" in i['realname']]
 
 brs = []
 for q in qsorts:
     addr = q['vaddr']
-    size = str(int(r2.cmdj("pdfj @ "+addr)['size']) // 4)
-    insts = r2.cmdj("pdj "+size+" @ "+addr)
-    for inst in insts:
-        if inst['type'] == "cjmp": brs.append(inst['vaddr'])
+    #size = str(int(r2.cmdj("pdfj @ "+addr)['size']) // 4)
+    insts = r2.cmdj("pdfj @ "+hex(addr))['ops']
+    for c, inst in enumerate(insts):
+        if inst['type'] == "cjmp": brs.append(hex(addr+(c*4)))
 
 total = 0
 weighted_total = 0
 for trace, weight in get_traces.get_trace_set("641.leela_s", "test"):
-    df = pl.load_parquet(trace_dir+trace)
+    df = pl.read_parquet(trace_dir+trace)
     trace_total = 0
     for inst_addr in df['inst_addr'].unique():
         if inst_addr in brs:
