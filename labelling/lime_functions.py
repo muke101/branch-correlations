25cc2848016e372bb47e63fc3670b43a6f111ec0
w.yin <w.yin@qmul.ac.uk>
2025-06-17 13:23:17 +0100

    added lime scripts to perturb instances

---
+++ labelling/lime_functions.py
@@ -0,0 +1,146 @@
+import torch
+import yaml
+import os
+import sys
+
+import lime
+from lime.lime_text import LimeTextExplainer
+from torch import nn
+
+sys.path.append(os.getcwd())  # which means the script should be run at labelling/
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # add the current directory
+sys.path.append(
+    os.path.dirname(os.path.abspath(__file__)) + "/../BranchNet/src/branchnet"
+)  # add the parent directory
+dir_tests = os.path.dirname(os.path.abspath(__file__)) + "/tests/"
+dir_config = (
+    os.path.dirname(os.path.abspath(__file__))
+    + "/../BranchNet/src/branchnet/configs/big.yaml"
+)
+
+from model import BranchNet
+from model import BranchNetTrainingPhaseKnobs
+from benchmark_branch_loader import BenchmarkBranchLoader
+
+
+def idx_to_string(idx: int) -> str:
+    """Convert an integer index to a string of the form '0x759:taken'."""
+    raw = format(idx, "b")
+    takenness = raw[-1]
+    pc_bin = raw[:-1]
+    pc_hex = hex(int(pc_bin, 2))
+    return "{}:{}".format(pc_hex, "taken" if takenness == "1" else "not_taken")
+
+
+def tensor_to_string(tensor: torch.Tensor) -> str:
+    """Convert a tensor of integers to a string of '0x759:taken 0x759:not_taken ...'."""
+    ret = ""
+    for x in tensor:
+        ret += idx_to_string(x) + " "
+    return ret.strip()
+
+
+def string_to_idx(tok: str) -> int:
+    """Convert a string of hex addresses to a tensor of integers.
+    The string should be in the format '0x759:taken 0x759:not_taken ...'."""
+
+    addr, takenness = tok.split(":")
+    addr_bin = format(int(addr, 16), "b")
+    takenness_bit = "1" if takenness == "taken" else "0"
+    return int(addr_bin + takenness_bit, 2)
+
+
+def string_to_tensor(string: str) -> torch.Tensor:
+    """Convert a string of hex addresses to a tensor of integers.
+    The string should be in the format '0x759:taken 0x759:not_taken ...'."""
+
+    tokens = string.split(" ")
+    return torch.tensor([string_to_idx(tok) for tok in tokens], dtype=torch.int64)
+
+
+class EvalWrapper:
+    def __init__(self, model: nn.Module) -> None:
+
+        self.model = model
+        self.model.eval()
+
+    def _probs(self, input_data) -> torch.Tensor:
+        """Get the probabilities of the input data using the model.
+        Args:
+            input_data (torch.Tensor): Input data of shape (batch_size, input_length).
+        """
+        with torch.no_grad():
+            output = self.model(input_data)
+            probs = torch.sigmoid(output)
+        return probs
+
+    def _prob_from_one_string(self, input_string: str) -> torch.Tensor:
+        """Get the probabilities of the input string using the model.
+        Args:
+            input_string (str): Input string of hex addresses in the format '0x759:taken 0x759:not_taken ...'.
+        """
+        input_tensor = string_to_tensor(input_string)
+        return self._probs(input_tensor.unsqueeze(0))
+
+    def probs_from_list_of_strings(self, input_strings: list[str]) -> torch.Tensor:
+        """Get the probabilities of a list of input strings using the model.
+        the output is a tensor of shape (batch_size, 2) where the first column is the probability of not taken and the second column is the probability of taken.
+        This output format is for matching the expected output of LimeTextExplainer, or the sklearn predict_proba method.
+        Args:
+            input_strings (list[str]): List of input strings of hex addresses in the format '0x759:taken 0x759:not_taken ...'.
+        """
+        input_tensors = [string_to_tensor(s) for s in input_strings]
+        positive_class_answer = self._probs(torch.stack(input_tensors))
+        negative_class_answer = 1 - positive_class_answer
+        return (
+            torch.stack([negative_class_answer, positive_class_answer])
+            .transpose(0, 1)
+            .numpy()
+        )
+
+    @staticmethod
+    def from_checkpoint(checkpoint_path, config_path=dir_config) -> "EvalWrapper":
+        """Load a model from a BranchNet checkpoint path and config and return an EvalWrapper instance."""
+
+        with open(config_path, "r") as f:
+            config = yaml.safe_load(f)
+
+        training_phase_knobs = BranchNetTrainingPhaseKnobs()
+        model = BranchNet(config, training_phase_knobs)
+        model.load_state_dict(torch.load(checkpoint_path))
+        model.eval()
+        return EvalWrapper(model)
+
+
+with open(dir_tests + "example_history.pt", "rb") as f:
+    test_input = torch.load(f)
+with open(dir_tests + "example_label.pt", "rb") as f:
+    test_label = torch.load(f)
+
+"""
+
+# Example usage of EvalWrapper and LimeTextExplainer with a BranchNet model.
+
+eval_wrapper = EvalWrapper.from_checkpoint(
+    dir_tests + "base_0x40b412_checkpoint.pt", config_path=dir_config
+)
+
+
+explainer = LimeTextExplainer(
+    class_names=["not_taken", "taken"],
+    char_level=False,
+    split_expression=lambda x: x.split(" "),
+    bow=False,
+    feature_selection="lasso_path",
+    mask_string="0x000:not_taken",  # Mask string for unknown addresses
+)
+exp = explainer.explain_instance(
+    tensor_to_string(test_input),
+    eval_wrapper.probs_from_list_of_strings,
+    num_features=5,
+    num_samples=2000,
+)
+
+print(exp.as_list())
+
+"""
--- labelling/lime_scratch.py
+++ labelling/lime_scratch.py
@@ -1,44 +1,137 @@
 import yaml
 import torch
 
-dir_results = '/mnt/data/results/branch-project/results/test/648.exchange2_s'
-dir_h5 = '/mnt/data/results/branch-project/datasets/648.exchange2_s'
-good_branches = ['0x429a78', '0x429b50']
+dir_results = "/mnt/data/results/branch-project/results-x86/test/641.leela_s"
+dir_h5 = "/mnt/data/results/branch-project/datasets-x86/641.leela_s"
+good_branches = [
+    0x417544,
+    0x417613,
+    0x4175DD,
+    0x4173B8,
+    0x4172EE,
+    0x415322,
+    0x41758C,
+    0x4175C2,
+    0x41774F,
+    0x417306,
+    0x4172FA,
+    0x417312,
+    0x41732A,
+    0x4171B7,
+    0x417762,
+    0x41731E,
+    0x40B883,
+    0x417652,
+    0x4175F8,
+    0x41763F,
+    0x40B614,
+    0x417571,
+    0x41534B,
+    0x4175A7,
+    0x417336,
+    0x41762A,
+    0x415398,
+    0x415374,
+    0x4177B1,
+    0x41539D,
+    0x417051,
+    0x41531D,
+    0x4178D4,
+    0x417735,
+    0x41834E,
+    0x40B853,
+    0x41737C,
+    0x40B4EF,
+    0x41821A,
+    0x40B97F,
+    0x41734B,
+    0x4173D4,
+    0x40B629,
+    0x415759,
+    0x41536F,
+    0x40B9B4,
+    0x40B5D0,
+    0x40B8FB,
+    0x413748,
+    0x416F53,
+    0x415346,
+    0x40B87E,
+    0x40B9AC,
+    0x4136BD,
+    0x41556F,
+    0x413760,
+    0x4177F1,
+    0x40B95F,
+    0x40B8ED,
+    0x40B5FC,
+    0x40B924,
+    0x40B412,
+    0x413754,
+    0x4177DA,
+    0x40B5E6,
+    0x415504,
+    0x415499,
+    0x413707,
+    0x4137AC,
+    0x40B968,
+    0x40B73C,
+    0x40B6B3,
+    0x40B932,
+    0x41822E,
+    0x41542C,
+    0x40B4A1,
+    0x41790B,
+    0x40B96D,
+    0x40B793,
+    0x40B63E,
+    0x40B8F6,
+    0x40B888,
+    0x4136FD,
+    0x415722,
+    0x40B92D,
+    0x4182AF,
+    0x40BA20,
+    0x418229,
+]
 
 USE_CUDA = False
 
 import os
 import sys
+
 sys.path.append(dir_results)
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd())  # which means the script should be run at labelling/
 
 from model import BranchNet
 from model import BranchNetTrainingPhaseKnobs
 from benchmark_branch_loader import BenchmarkBranchLoader
 
-dir_ckpt = dir_results + '/checkpoints'
-dir_config = dir_results + '/config.yaml'
+dir_ckpt = dir_results + "/checkpoints"
+dir_config = dir_results + "/config.yaml"
 
-with open(dir_config, 'r') as f:
+with open(dir_config, "r") as f:
     config = yaml.safe_load(f)
 
 training_phase_knobs = BranchNetTrainingPhaseKnobs()
 model = BranchNet(config, training_phase_knobs)
 if USE_CUDA:
-    model.to('cuda')
+    model.to("cuda")
 
 for good_branch in good_branches:
     # Load the model checkpoint
-    dir_ckpt = dir_results + '/checkpoints/' + 'base_{}_checkpoint.pt'.format(good_branch)
-    print('Loading model from:', dir_ckpt)
+    good_branch = hex(int(str(0x40B9B4)))
+    dir_ckpt = (
+        dir_results + "/checkpoints/" + "base_{}_checkpoint.pt".format(good_branch)
+    )
+    print("Loading model from:", dir_ckpt)
     model.load_state_dict(torch.load(dir_ckpt))
     model.eval()
- 
-    loader = BenchmarkBranchLoader('648.exchange2_s', good_branch, 'test')
-    print('Branch:', good_branch)
-    print('Instances:', len(loader))
+
+    loader = BenchmarkBranchLoader("641.leela_s", good_branch, "test")
+    print("Branch:", good_branch)
+    print("Instances:", len(loader))
     history, label = loader.get_instance(0)
-    print('Example:', history, label)
+    print("Example:", history, label)
 
     with torch.no_grad():
         history = history.unsqueeze(0)
@@ -47,8 +140,5 @@ for good_branch in good_branches:
             label = label.cuda()
         output = model(history)
         probs = torch.sigmoid(output)
-        
-        # print('Model output:', output)
 
-        
-
+        print("Model output:", probs)
