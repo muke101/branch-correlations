2d0259af9a656a497388e0ac564b23693bc59f34
muke101 <muke@protonmail.com>
2025-10-07 16:44:36 +0100

    fixed build tage

--- BranchNet/bin/build_tage.py
+++ BranchNet/bin/build_tage.py
@@ -9,7 +9,7 @@ tage_dir_path = os.path.dirname(os.path.abspath(__file__)) + '/../src/tage'
 print(__file__)
 print(build_dir_path)
 os.makedirs(build_dir_path, exist_ok=True)
-subprocess.call('cd {build_dir}; cmake3 {tage_dir} -DCMAKE_BUILD_TYPE=Release; make -j'.format(
+subprocess.call('cd {build_dir}; cmake {tage_dir} -DCMAKE_BUILD_TYPE=Release; make -j'.format(
     build_dir=build_dir_path,
     tage_dir=tage_dir_path,
 ), shell=True)
